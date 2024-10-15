fc05847db69a983249a4349879d5c73ff49173ad
Julien Danh <julien@Juliens-Air.lan>
2024-10-16 01:15:52 +0200

    add windows navigations

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Window navigation
+vim.keymap.set("n", "sh", "<C-w>h", { silent = true })
+vim.keymap.set("n", "sj", "<C-w>j", { silent = true })
+vim.keymap.set("n", "sk", "<C-w>k", { silent = true })
+vim.keymap.set("n", "sl", "<C-w>l", { silent = true })
+
+-- Split windows
+vim.keymap.set("n", "sv", ":vsplit<CR>", { silent = true }) -- Vertical split
+vim.keymap.set("n", "ss", ":split<CR>", { silent = true }) -- Horizontal split
