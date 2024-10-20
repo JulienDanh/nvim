1bc9ea02435867ebb47867e8e3b23e34d799a420
Julien Danh <danh.julien@gmail.com>
2024-10-20 14:20:18 +0200

    add cyberdream theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "cyberdream" } },
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     -- dap
