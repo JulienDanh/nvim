ad807982636cc4efd245853ce7e04dd05906c878
Julien Danh <danh.julien@gmail.com>
2024-10-20 14:50:42 +0200

    fix neotree shortcut conflict

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local opts = { noremap = true, silent = true }
 -- Window navigation
-vim.keymap.set("n", "sh", "<C-w>h", { silent = true })
-vim.keymap.set("n", "sj", "<C-w>j", { silent = true })
-vim.keymap.set("n", "sk", "<C-w>k", { silent = true })
-vim.keymap.set("n", "sl", "<C-w>l", { silent = true })
+vim.keymap.set("n", "sh", "<C-w>h", opts)
+vim.keymap.set("n", "sj", "<C-w>j", opts)
+vim.keymap.set("n", "sk", "<C-w>k", opts)
+vim.keymap.set("n", "sl", "<C-w>l", opts)
 
 -- Split windows
-vim.keymap.set("n", "sv", ":vsplit<CR>", { silent = true }) -- Vertical split
-vim.keymap.set("n", "ss", ":split<CR>", { silent = true }) -- Horizontal split
+vim.keymap.set("n", "sv", ":vsplit<CR>", opts) -- Vertical split
+vim.keymap.set("n", "ss", ":split<CR>", opts) -- Horizontal split
