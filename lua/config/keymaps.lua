3e7acf39e99b199bbdc865a760b06311130ee7dd
Julien Danh <danh.julien@gmail.com>
2025-08-08 14:45:56 +0200

    clean

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,14 +3,5 @@
 -- Add any additional keymaps here
 
 local opts = { noremap = true, silent = true }
--- Window navigation
-vim.keymap.set("n", "sh", "<C-w>h", opts)
-vim.keymap.set("n", "sj", "<C-w>j", opts)
-vim.keymap.set("n", "sk", "<C-w>k", opts)
-vim.keymap.set("n", "sl", "<C-w>l", opts)
-
--- Split windows
-vim.keymap.set("n", "sv", ":vsplit<CR>", opts) -- Vertical split
-vim.keymap.set("n", "ss", ":split<CR>", opts) -- Horizontal split
 
 vim.keymap.set("n", "q", "<Nop>", { noremap = true, silent = true })
